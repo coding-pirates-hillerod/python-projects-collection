b5578d881662dee24549feae8837076027cce8a4
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-22 16:26:43 +0200

    Added description for number guessing game

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -1,4 +1,13 @@
 """
 I denne opgave skal du lave et spil, hvor computeren genererer et tilfældigt tal
 mellem 1 og 10, som en bruger så skal gætte i kun 3 forsøg.
+
+For at løse denne opgave skal du vide noget om:
+- random modulet
+- Boolean datatypen
+- print funktionen
+- while loops
+- input funktionen
+- if, elif, else statements
+- tildelingsoperatorer (eng: 'assignment operators')
 """
