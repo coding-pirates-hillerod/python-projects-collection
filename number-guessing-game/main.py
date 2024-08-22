daae93e0c075eb9af88b4a5df417ed054299a55a
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-22 16:37:56 +0200

    Improved project descriptions

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -1,7 +1,9 @@
 """
+*** OPGAVEN ***
 I denne opgave skal du lave et spil, hvor computeren genererer et tilfældigt tal
 mellem 1 og 10, som en bruger så skal gætte i kun 3 forsøg.
 
+*** HJÆLP TIL OPGAVEN ***
 For at løse denne opgave skal du vide noget om:
 - random modulet
 - Boolean datatypen
--- password-generator/main.py
+++ password-generator/main.py
@@ -1,3 +1,12 @@
 """
-Din opgave er at skrive noget kode som genererer et password bestående af 10 tilfældige tegn af bogstaver og tal
+*** OPGAVEN ***
+Din opgave er at skrive noget kode som genererer et password bestående af 10 tilfældige tegn af bogstaver og tal.
+
+*** HJÆLP TIL OPGAVEN ***
+For at løse denne opgave kan det være værd at vide noget om:
+- string modulet
+- random modulet
+- list funktionen
+- 'join' metoden på tekststrenge
+- print funktionen
 """
