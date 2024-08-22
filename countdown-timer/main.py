39cea49363e83fd55e02836624d258c5a56be45a
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-22 17:18:12 +0200

    Added count down timer project

---
+++ countdown-timer/main.py
@@ -0,0 +1,12 @@
+"""
+*** OPGAVEN ***
+I denne opgave skal du lave en "count down timer" fra 10 til 0 - altså noget kode
+som tæller ned fra 10 til 0 med en pause på 1 sekund mellem hvert tal.
+
+*** HJÆLP ***
+For at løse opgaven skal du vide noget om:
+- 'sleep()' funktionen fra 'time' module
+- 'while' loops
+- print funktionen
+- Tildelingsoperator (her: -=)
+"""
---
+++ countdown-timer/solution/main.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+n = 10
+
+while n >= 0:
+    print(n)
+    sleep(1)
+    n -= 1
