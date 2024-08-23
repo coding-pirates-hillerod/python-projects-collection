12a0060f96f02402c0b8d4df7fbc366e04bbb0dc
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-23 14:04:35 +0200

    Added project for even or odd number

---
+++ even-or-odd-number/main.py
@@ -0,0 +1,12 @@
+"""
+*** OPGAVE ***
+I denne opgave opgave skal du bede brugeren af programmet om et tal,
+og så efterfølgende printe ud, hvorvidt tallet er lige eller ulige.
+
+*** HÆLP ***
+For at løse denne opgave skal du vide noget om:
+- input funktionen
+- int funktionen
+- if/else statements
+- modulos operatoren
+"""
---
+++ even-or-odd-number/solution/main.py
@@ -0,0 +1,6 @@
+num = int(input("Indtast et tal: "))
+
+if (num % 2) == 0:
+    print("Taller er et lige tal")
+else:
+    print("Tallet er ulige")
